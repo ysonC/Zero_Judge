64803e0844dec7e1a3230106c84da9642605989d
Wyson <efyhc5@nottingham.ac.uk>
2021-02-10 12:43:42 +0800

    a015

--- a015.cpp
+++ a015.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 
 using namespace std;
-
+//2 3 
+//                  3 8
+//3 1 2    ==>      1 5
+//8 5 4             2 4
 int main()
 {
-    int number[5] = {0, 1, 2, 3, 4};
-    int length = sizeof(number) / sizeof(number[0]);
+    int a, b;
+    cin >> a >> b; // rows ;  column
+    int maze[100][100];
 
-    for(int i = 0; i < length; i++) {
-        cout << number[i] << " "; 
+    if (a > 100 || b > 100)
+        return 1;
+
+    for(int row = 0; row < a; row++) 
+    {
+        for(int i = 0; i < b; i++) 
+        {
+            cin >> maze[row][i];
+        }
     }
-    cout << endl; 
 
-    return 0; 
+    for(int row = 0; row < b; row++) 
+    {
+        for(int i = 0; i < a; i++) 
+        {
+            cout << maze[i][row] << " "; 
+        }
+        cout << endl; 
+    } 
+
+     
 }
